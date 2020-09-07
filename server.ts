efaf3fe4716585e5cd5649300e95e213edb2b357
Dawid Dahl <dawiddahl@gmail.com>
2020-09-07 19:19:41 +0200

    changed cors settings

--- server.ts
+++ server.ts
@@ -9,12 +9,7 @@ export const app = express();
 const PORT = process.env.PORT || 1234;
 
 app.use(morgan("dev"));
-app.use(
-	cors({
-		origin: "http://localhost:1234",
-		credentials: true,
-	})
-);
+app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("dist"));
