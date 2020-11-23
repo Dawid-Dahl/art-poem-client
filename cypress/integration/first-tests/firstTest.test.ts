adde6038e6a8586a75ac0ec7ca57c875fa7df663
Dawid Dahl <dawiddahl@gmail.com>
2020-11-23 21:24:10 +0100

    added cypress assertions

--- cypress/integration/first-tests/firstTest.test.ts
+++ cypress/integration/first-tests/firstTest.test.ts
@@ -1,9 +1,14 @@
+import {includes} from "cypress/types/lodash";
+
 describe("First Test", () => {
 	describe("happy path", () => {
 		it("should log into the app", () => {
-			cy.visit("https://art-poem-client.herokuapp.com/");
+			cy.visit("https://art-poem-client.herokuapp.com/login");
 			cy.get("#email").type("dawiddahl@gmail.com");
+			cy.url().should("equal", "https://art-poem-client.herokuapp.com/login");
 			cy.get("#password").type(Cypress.env("password")).type("{enter}");
+			cy.wait(2000);
+			cy.get("h1").contains("Discover").should("be.visible");
 		});
 	});
 });
