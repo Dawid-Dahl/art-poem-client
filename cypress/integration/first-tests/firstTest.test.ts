9d2a709de6ef21dfd87d26a7c8e04156bbbe5bd9
Dawid Dahl <dawiddahl@gmail.com>
2020-11-23 20:34:14 +0100

    added cypress env vars

--- cypress/integration/first-tests/firstTest.test.ts
+++ cypress/integration/first-tests/firstTest.test.ts
@@ -1,9 +1,9 @@
 describe("First Test", () => {
 	describe("happy path", () => {
-		it("should log into the app", async () => {
+		it("should log into the app", () => {
 			cy.visit("https://art-poem-client.herokuapp.com/");
 			cy.get("#email").type("dawiddahl@gmail.com");
-			cy.get("#password").type("asdasd").type("{enter}");
+			cy.get("#password").type(Cypress.env("password")).type("{enter}");
 		});
 	});
 });
